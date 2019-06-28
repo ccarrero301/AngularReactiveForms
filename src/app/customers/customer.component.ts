0ba4c68749191f39e788411b29c4eb364a957a54
Carlos Carrero <carlos.carrero@gmail.com>
2019-06-27 22:39:39 -0500

    Initial commit

---
+++ src/app/customers/customer.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { Customer } from './customer';
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.css']
+})
+export class CustomerComponent implements OnInit {
+
+  customerForm: FormGroup;
+  customer = new Customer();
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+
+    this.customerForm = this.formBuilder.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.email]],
+      sendCatalog: true
+    });
+  }
+
+  populateTestData(): void {
+    this.customerForm.setValue({
+      firstName: 'Jack',
+      lastName: 'Harkness',
+      email: 'jack@torchwood.com',
+      sendCatalog: false
+    });
+  }
+
+  save() {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+}
