92730f1ef7a70893e043775e3ad7823f3dc93903
Carlos Carrero <carlos.carrero@gmail.com>
2019-06-28 08:11:55 -0500

    Adding validation rules at run time

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -21,6 +21,8 @@ export class CustomerComponent implements OnInit {
       firstName: ['', [Validators.required, Validators.minLength(3)]],
       lastName: ['', [Validators.required, Validators.maxLength(50)]],
       email: ['', [Validators.required, Validators.email]],
+      phone: '',
+      notification: 'email',
       sendCatalog: true
     });
   }
@@ -30,6 +32,8 @@ export class CustomerComponent implements OnInit {
       firstName: 'Jack',
       lastName: 'Harkness',
       email: 'jack@torchwood.com',
+      phone: '',
+      notification: 'email',
       sendCatalog: false
     });
   }
@@ -37,5 +41,17 @@ export class CustomerComponent implements OnInit {
   save() {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  setNotification(notifyVia: string) {
+    const phoneControl = this.customerForm.get('phone');
+
+    if (notifyVia === 'text') {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+
+    phoneControl.updateValueAndValidity();
   }
 }
